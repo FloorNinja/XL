6f08814945fcfba5026af359bcbc8db5c86e9571
Axel Österman <dat14aos@student.lu.se>
2016-05-17 16:38:01 +0200

    Kopierade in "rätt" kod till GUI.menu, det finns error här men det beror på att CurrenSlot inte finns i GUI ännu samt några metoder i XL.java (get/set Data)

--- src/gui/menu/SaveMenuItem.java
+++ src/gui/menu/SaveMenuItem.java
@@ -2,6 +2,8 @@ package gui.menu;
 
 import gui.StatusLabel;
 import gui.XL;
+import io.XLPrintStream;
+
 import java.io.FileNotFoundException;
 import javax.swing.JFileChooser;
 
@@ -9,9 +11,14 @@ class SaveMenuItem extends OpenMenuItem {
     public SaveMenuItem(XL xl, StatusLabel statusLabel) {
         super(xl, statusLabel, "Save");
     }
-
+    
+    /* here we have to open the file specified by "path" and
+     * then write all the slots to it as specified in the assignment.
+     * use XLPrintStream for writing.
+     */
     protected void action(String path) throws FileNotFoundException {
-        // TODO
+    	XLPrintStream print = new XLPrintStream(path);
+    	print.save(xl.getData());
     }
 
     protected int openDialog(JFileChooser fileChooser) {
