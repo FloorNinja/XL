52a6fa731c6c515f9d759c0c1b34092306c40ee9
Simon Wictorsson <simpa@wictorsson.com>
2016-05-17 16:55:32 +0200

    Added comment to the code.

--- src/model/ExpressionSlot.java
+++ src/model/ExpressionSlot.java
@@ -7,25 +7,40 @@ public class ExpressionSlot implements Slot {
 	
 	private Expr expr;
 	
+	/**
+	 * Constructor
+	 * @param expr
+	 */
 	public ExpressionSlot(Expr expr) {
 		this.expr = expr;
 	}
-
+	
+	/**
+	 * Returns the value of the Expression
+	 * @param e
+	 */
 	@Override
 	public double getValue(Environment e) {
-		// TODO Auto-generated method stub
 		return expr.value(e);
 	}
 	
+	/**
+	 * Returns a String of the value of the Expression
+	 * @param e
+	 */
 	@Override
 	public String stringValue(Environment env) {
-		// TODO Auto-generated method stub
 		return Double.toString(expr.value(env));
 	}
 
+	/**
+     * The <code>toString</code> method returns a <code>String</code>
+     * representation of this expression without unnecessary parentheses.
+     * 
+     * @return the <code>String</code> representation of this expression.
+     */
 	@Override
 	public String toString(Environment e) {
-		// TODO Auto-generated method stub
 		return expr.toString();
 	}
 
