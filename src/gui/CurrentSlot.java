632abc72780cf1dd49383513b7f086ca7e99fda2
FloorNinja <ackekuttner@gmail.com>
2016-05-23 15:47:47 +0200

    Something is fishy about ClearMenuItem :O

--- src/gui/CurrentSlot.java
+++ src/gui/CurrentSlot.java
@@ -25,14 +25,6 @@ public class CurrentSlot extends Observable {
 	public String getAddress() {
 		return currentSlot.getAddress();
 	}
-	
-	/**
-	 * Sets the text for the current label
-	 * @param text
-	 */
-	public void setText(String text) { //TODO: Denna kommer antagligen försvinna
-		currentSlot.setText(text);
-	}
 
 	/**
 	 * Set the background to white again.
--- src/gui/SlotLabels.java
+++ src/gui/SlotLabels.java
@@ -2,22 +2,25 @@ package gui;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.SwingConstants;
 
 import model.Sheet;
+import model.Slot;
 
 public class SlotLabels extends GridPanel implements Observer {
-    private List<SlotLabel> labelList;
+    private HashMap<String, SlotLabel> slotMap;
     private CurrentSlot currentSlot;
     private Sheet sheet;
 
     public SlotLabels(int rows, int cols, CurrentSlot currentSlot, Sheet sheet) {
         super(rows + 1, cols);
-        labelList = new ArrayList<SlotLabel>(rows * cols);
+        slotMap = new HashMap<String, SlotLabel>();
         this.currentSlot = currentSlot;
         this.sheet = sheet;
         sheet.addObserver(this);
@@ -37,16 +40,22 @@ public class SlotLabels extends GridPanel implements Observer {
             	
                 SlotLabel label = new SlotLabel(address, currentSlot);
                 add(label);
-                labelList.add(label);
+                slotMap.put(address, label);
             }
         }
-        SlotLabel firstLabel = labelList.get(0);
+        SlotLabel firstLabel = slotMap.get("A1");
         //Set the firstLabel as the currentSlot here.
         currentSlot.set(firstLabel);
     }
 
 	@Override
 	public void update(Observable obs, Object arg) {
-		
+		for(Entry<String, SlotLabel> entry: slotMap.entrySet()){
+			entry.getValue().setText("");
+		}
+		for(Entry<String, Slot> entry : sheet.getEntries()) {
+			String address = entry.getKey();
+			slotMap.get(address).setText(sheet.slotStringValue(address));
+		}
 	}
 }
